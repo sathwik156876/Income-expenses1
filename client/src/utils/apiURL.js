40627debeda3420d2c9edc4bb3542b89945d1dfd
Sathwik Reddy <141897331+sathwik156876@users.noreply.github.com>
2023-08-24 12:40:16 +0530

    apiURL.js

--- client/src/utils/apiURL.js
+++ client/src/utils/apiURL.js
@@ -1,3 +1,3 @@
-export const API_URL_USER = "http://localhost:8080/api/v1/users";
-export const API_URL_ACC = "http://localhost:8080/api/v1/accounts";
-export const API_URL_TRANSACTION = "http://localhost:8080/api/v1/transactions";
+export const API_URL_USER = "https://income-expenses1.onrender.com/api/v1/users";
+export const API_URL_ACC = "https://income-expenses1.onrender.com/api/v1/accounts";
+export const API_URL_TRANSACTION = "https://income-expenses1.onrender.com/api/v1/transactions";
